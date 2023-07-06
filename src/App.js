6f1001814f7a2266baa185e13a43adfb80905b1d
Hamza-Noah <hamza.nouh97@gmail.com>
2023-07-06 15:59:43 +0300

    added sidebar and navbar compnents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <div className="row m-0">
+        <div className="col-2 sidebar"><Sidebar /></div>
+        <div className="col-10">
+          <Navbar />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <ul className="list-unstyled">
+      <li>
+        <a href="">Get All Products</a>
+      </li>
+      <li>
+        <a href="">Get All Categories</a>
+      </li>
+    </ul>
+  );
+}
